20e9860dcad9d3a237b4b8abc9a157956852e2b9
masudripon <masud52sec@gmail.com>
2015-08-14 15:32:10 -0700

    new file test.html created and some changes in index.php file

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
-<?php
+<html>
+    <head><title>
+        
+    </title>
+    </head>
+    <body>
+        <ul>
+            <li>list no 1</li>
+            <li>list no 2</li>
+            <li>list no 3</li>
+            <li>list no 4</li>
+           
+        </ul>
+    </body>
+</html>
+    <?php
 echo "hello world";
 
 ?>
